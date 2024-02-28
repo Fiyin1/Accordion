3a013fe088af1205fea46d112ceda6b56a86eb83
Fiyin1 <fiyinabdul@gmail.com>
2024-02-28 16:58:44 +0100

    accordion

---
+++ src/components/accordion/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id: '1',
+        question: "What is music?",
+        answer:
+            "vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion"
+    }, {
+        id: '2',
+        question: "What is Music Notation",
+        answer: "Notation is used to write down music. It's one way to remember what you have composed. It's also great in helping other musicians read your music and play it for themselves."
+    }, {
+        id: '3',
+        question: "Accordion is a musical instrument",
+        answer: "The accordion is a musical instrument played by stretching and squeezing with the hands to work a central bellows that blows air over metal reeds, the melody and chords being sounded by buttons or keys."
+    }, {
+        id: '4',
+        question: "What is music theory",
+        answer: "Music theory is the study of how music works, and its fundamental elements and principles, including melody, harmony, rhythm, and structure."
+    },
+];
+
+export default data;
